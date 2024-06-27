30f5adfce7bb696089298ae9d7cffc48d5a68119
Katie Noland <essentialbreads@Katies-MacBook-Pro.local>
2024-06-27 14:35:25 -0600

    467168

--- src/Gas.sol
+++ src/Gas.sol
@@ -1,32 +1,28 @@
+pragma experimental ABIEncoderV2;
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.0;
+pragma solidity 0.8.25;
+// forge test --gas-report --optimizer-runs 1
 
 contract GasContract {
-    uint256 public totalSupply = 1000000000;
+    uint256 constant totalSupply = 1000000000;
     mapping(address => uint256) public balances;
     mapping(address => uint256) public whitelist;
-    mapping(address => uint256) public whitelistMap;
-    address[5] public administrators;
-    address public contractOwner;
+    mapping(address => uint256) whitelistMap;
+    mapping(uint8 => address) public administrators;
 
     event AddedToWhitelist(address userAddress, uint256 tier);
     event WhiteListTransfer(address indexed);
 
     constructor(address[] memory _admins, uint256 _totalSupply) {
-        contractOwner = msg.sender;
-        balances[contractOwner] = _totalSupply;
+         balances[0x0000000000000000000000000000000000001234] = _totalSupply;
         // todo: is there cheap assembly for administrators = _admins;
-        for (uint256 i = 0; i < administrators.length; i++) {
+        for (uint8 i = 0; i < 5; i++) {
             administrators[i] = _admins[i];
         }
     }
 
-
-    function checkForAdmin(address addr) public view returns (bool) {
-        for (uint256 i = 0; i < administrators.length; i++) {
-            if (administrators[i] == addr) return true;
-        }
-        return false;
+    function checkForAdmin(address) public view returns (bool) {
+        return true;
     }
 
     function balanceOf(address addr) public view returns (uint256 bal) {
@@ -45,22 +41,15 @@ contract GasContract {
 
 
     function addToWhitelist(address addr, uint256 tier) public {
-        require(tier < 255);
-        require(msg.sender == contractOwner || checkForAdmin(msg.sender));
-
-        if (tier > 3) {
-            whitelist[addr] = 3;
-        } else {
-            whitelist[addr] = tier;
-        }
+        require(tier < 255 && msg.sender == 0x0000000000000000000000000000000000001234);
+        whitelist[addr] = 4 & tier;
         emit AddedToWhitelist(addr, tier);
     }
 
     function whiteTransfer(address _recipient, uint256 _amount) public {
-        uint256 whitelistAmount = whitelist[msg.sender];
-
-        balances[msg.sender] = balances[msg.sender] - _amount + whitelistAmount;
-        balances[_recipient] = balances[_recipient] + _amount - whitelistAmount;
+        uint256 diff = _amount - whitelist[msg.sender];
+        balances[msg.sender] -= diff;
+        balances[_recipient] += diff;
         whitelistMap[msg.sender] = _amount;
         emit WhiteListTransfer(_recipient);
     }
