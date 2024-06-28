fe1a933d19807e189d6a5ca36af2acce41d86ce2
Katie Noland <essentialbreads@Katies-MacBook-Pro.local>
2024-06-28 14:39:04 -0600

    456182

--- src/Gas.sol
+++ src/Gas.sol
@@ -1,8 +1,7 @@
-pragma experimental ABIEncoderV2;
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.25;
 // forge test --gas-report --optimizer-runs 1
-
+// 456182
 contract GasContract {
     uint256 constant totalSupply = 1000000000;
     mapping(address => uint256) public balances;
@@ -14,11 +13,12 @@ contract GasContract {
     event WhiteListTransfer(address indexed);
 
     constructor(address[] memory _admins, uint256 _totalSupply) {
-         balances[0x0000000000000000000000000000000000001234] = _totalSupply;
+        balances[0x0000000000000000000000000000000000001234] = _totalSupply;
         // todo: is there cheap assembly for administrators = _admins;
         for (uint8 i = 0; i < 5; i++) {
             administrators[i] = _admins[i];
         }
+
     }
 
     function checkForAdmin(address) public view returns (bool) {
@@ -34,15 +34,38 @@ contract GasContract {
         }
     }
 
-    function transfer(address _recipient, uint256 _amount, string calldata _name) public {
-        balances[msg.sender] -= _amount;
-        balances[_recipient] += _amount;
+    function transfer(address recipient, uint256 amt, string calldata _name) public {
+        assembly {
+        // balances[msg.sender] -= amt;
+            mstore(0x0, caller())
+            mstore(0x20, balances.slot)
+            let mapSlot := keccak256(0x0, 0x40)
+            let mapValue := sload(mapSlot)
+            let newValue := sub(mapValue, amt)
+            sstore(mapSlot, newValue)
+
+        // balances[_recipient] += amt;
+            mstore(0x0, recipient)
+            mapSlot := keccak256(0x0, 0x40)
+            mapValue := sload(mapSlot)
+            newValue := add(mapValue, amt)
+            sstore(mapSlot, newValue)
+        }
     }
 
 
     function addToWhitelist(address addr, uint256 tier) public {
         require(tier < 255 && msg.sender == 0x0000000000000000000000000000000000001234);
-        whitelist[addr] = 4 & tier;
+        assembly {
+//            if or(lt(tier, 255), not(eq(caller(), 0x0000000000000000000000000000000000001234))) {revert(0, 0)}
+        // whitelist[addr] = 4 & tier;
+            mstore(0x0, addr)
+            mstore(0x20, whitelist.slot)
+            let whitelistSlot := keccak256(0x0, 0x40)
+            let whitelistValue := sload(whitelistSlot)
+            let newValue := and(4, tier)
+            sstore(whitelistSlot, newValue)
+        }
         emit AddedToWhitelist(addr, tier);
     }
 
