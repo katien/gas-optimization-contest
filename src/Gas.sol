73a9eca4609759f378f8da52f45a4871629cc5a1
Katie Noland <essentialbreads@Katies-MacBook-Pro.local>
2024-06-29 11:16:10 -0600

    fix bug 304678

--- src/Gas.sol
+++ src/Gas.sol
@@ -6,7 +6,7 @@ pragma solidity 0.8.4;
 *
 * Command: `forge test --gas-report --optimizer-runs 1`
 * Current Score:
-* 304438
+* 304678
 */
 contract GasContract {
     uint256 constant totalSupply = 1000000000;
@@ -14,18 +14,18 @@ contract GasContract {
     // store administrator balance
     constructor(address[] memory _admins, uint256 _totalSupply) payable {
         assembly {
-            sstore(0x0000000000000000000000000000000000001234, _totalSupply)
+            sstore(0x1234, _totalSupply)
         }
     }
     // get hardcoded administrators
-    function administrators(uint8 _index) external view returns (address admin_) {
+    function administrators(uint8 _index) external view returns (address _admin) {
         assembly {
             switch _index
-            case 0 {admin_ := 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2}
-            case 1 {admin_ := 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46}
-            case 2 {admin_ := 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf}
-            case 3 {admin_ := 0xeadb3d065f8d15cc05e92594523516aD36d1c834}
-            case 4 {admin_ := 0x1234}
+            case 0 {_admin := 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2}
+            case 1 {_admin := 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46}
+            case 2 {_admin := 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf}
+            case 3 {_admin := 0xeadb3d065f8d15cc05e92594523516aD36d1c834}
+            case 4 {_admin := 0x1234}
         }
     }
 
@@ -83,9 +83,10 @@ contract GasContract {
             sstore(mapSlot, sub(sload(mapSlot), value))
 
         // recipient.balance = recipient.balance + value
+
             mapSlot := recipient
         // todo: SLOADing from the mapSlot variable currently costs more than running the hash again, verify this does not change before submitting final version
-            sstore(mapSlot, add(mapSlot, value))
+            sstore(mapSlot, add(sload(mapSlot), value))
         }
     }
 
